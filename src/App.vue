67d920c0aecb3e0efaff40c0d94130d4047e7498
zhang260192zj <2601920118@qq.com>
2023-02-08 17:52:28 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <component :is="hello"></component>
+</template>
+
+<script>
+import { defineComponent} from 'vue'
+import HelloWorld from './components/HelloWorld.vue';
+export default defineComponent({
+  setup () {
+    return {
+      hello:HelloWorld
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+ <h1>{{a}}</h1>
+ <button @click="add">Add</button>
+</template>
+<script>
+import { defineComponent ,reactive,ref} from 'vue'
+
+export default defineComponent({
+  setup () {
+    let a = ref(1)
+    const obj = {
+      a:1
+    }
+    let b =reactive(obj)
+    console.log(obj,b)
+    const add =()=>{
+
+    }
+    return {
+      a,
+      obj,
+      add,
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
